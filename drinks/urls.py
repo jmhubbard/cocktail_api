8fb8bfa88088695166cf16be742c8a2934eb4860
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 21:49:21 -0700

    add drink-list url

--- drinks/urls.py
+++ drinks/urls.py
@@ -18,4 +18,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
+    path('drink-list/', views.drinkList, name='drink-list'),
 ]
