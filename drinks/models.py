0d983ff6ffdd3a8be3bcf64ec343f7b19e7cd31a
Jason Hubbard <jasonhubb@gmail.com>
2020-10-29 10:46:37 -0700

    set Drink name field to be unique

--- drinks/models.py
+++ drinks/models.py
@@ -3,7 +3,7 @@ from ingredients.models import Ingredient
 from recipes.models import Recipe
 
 class Drink(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     instructions = models.TextField()
     ingredients = models.ManyToManyField(Ingredient, through='recipes.Recipe')
     
