7f2d8cc7f38262947a79f8ce61a47c20728c75da
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 19:46:25 -0700

    add ingredients to installed_apps

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'drinks.apps.DrinksConfig',
+    'ingredients.apps.IngredientsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
