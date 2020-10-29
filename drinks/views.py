88b9019ea511b62f17b0e592416e461849b66f28
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 20:50:22 -0700

    create apiOverview view

--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/drink-list/',
+        'Detail View': '/drink-detail/<str:pk>/',
+        'Create': '/drink-create/',
+        'Update': '/drink-update/<str:pk>/',
+        'Delete': '/drink-delete/<str:pk>/',
+    }
+    return Response(api_urls)
