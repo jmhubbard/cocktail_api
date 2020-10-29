c3370846fba225799dedb7d26160520aec7fad43
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 19:55:52 -0700

    create IngredientAdmin

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient
+
+
+class IngredientAdmin(admin.ModelAdmin):
+
+    list_display = ('name',)
+    list_filter = ('name',)
+
+admin.site.register(Ingredient, IngredientAdmin)
