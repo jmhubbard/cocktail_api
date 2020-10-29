ea836ba8941490ce5670c1edad387cd53ef3ac37
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 20:18:41 -0700

    create Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from drinks.models import Drink
+from ingredients.models import Ingredient
+
+class Recipe(models.Model):
+    drink = models.ForeignKey(
+        Drink,
+        on_delete=models.CASCADE,
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+    )
+    amount = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.amount} {self.ingredient}'
