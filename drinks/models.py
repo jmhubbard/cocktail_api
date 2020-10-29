a385e3a65079abe785701a878d53287fb1224305
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 21:48:03 -0700

    add many-to-many field to link ingredients through recipe

--- drinks/models.py
+++ drinks/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from ingredients.models import Ingredient
+from recipes.models import Recipe
 
 class Drink(models.Model):
     name = models.CharField(max_length=200)
     instructions = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through='recipes.Recipe')
     
     def __str__(self):
         return self.name
