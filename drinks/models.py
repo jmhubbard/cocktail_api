de200376b79d5ffa60231493fbec4245baae4247
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 19:59:17 -0700

    Create initial Drink model

--- drinks/models.py
+++ drinks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=200)
+    instructions = models.TextField()
+    
+    def __str__(self):
+        return self.name
