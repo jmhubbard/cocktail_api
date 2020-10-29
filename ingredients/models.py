d44c7a77bf56e39c058598e7fd2cd66efb7911c2
Jason Hubbard <jasonhubb@gmail.com>
2020-10-28 19:51:54 -0700

    Create Ingredient model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
