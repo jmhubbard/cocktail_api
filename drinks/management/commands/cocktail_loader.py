a6afcb09a1086bd617710e83904b2db4b41daa65
Jason Hubbard <jasonhubb@gmail.com>
2020-10-29 17:07:39 -0700

    add ability to load all ingredients

--- drinks/management/commands/cocktail_loader.py
+++ drinks/management/commands/cocktail_loader.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
 from drinks.models import Drink
+from ingredients.models import Ingredient
 
 from drinks.cocktailsdict import cocktailsdict
 
@@ -16,32 +17,50 @@ class Command(BaseCommand):
         savedDrink = 0
         duplicateDrink = 0
         #Character Counts
+        savedIngredient = 0
+        duplicateIngredient = 0
 
 
         for item in cocktailsdict:
             totalAttemptedItems += 1
-            #Create an instance of Show using the provided show name
+            #Create an instance of Drink using the provided drink name and instructions
             drink = Drink(
                 name = item["strDrink"],
                 instructions = item["strInstructions"],
             )
             if options["save"]:
                 try:
-                    #Save the show instance if it doesn't exist in database
+                    #Save the drink instance if it doesn't exist in database
                     drink.save()
                 except IntegrityError:
-                    #If the show already exists then get that shows object to use when saving episodes
+                    #If the drink already exists then get that drink object to use when saving recipe
                     duplicateDrink += 1
                     drink = Drink.objects.get(name=item["strDrink"])
                 else:
                     savedDrink += 1
-                # finally:
-                #     #Create an instance of Episode using the provided episode name, season and the previous show instance
-                #     episode = Episode(
-                #         name = item["episode"],
-                #         season = item["season"],
-                #         show = show
-                #     )
+                finally:
+                    ingredientList = []
+                    for i in range(1,16):
+                        ingredientkey = f'strIngredient{i}'
+                        ingredientList.append(item[ingredientkey])
+
+                    for ingredient in ingredientList:
+                        if ingredient != None:
+                            newingredient = Ingredient(
+                                name = ingredient
+                            )
+                            try:
+                                #Save the drink instance if it doesn't exist in database
+                                newingredient.save()
+                            except IntegrityError:
+                                #If the drink already exists then get that drink object to use when saving recipe
+                                duplicateIngredient += 1
+                                newingredient = Ingredient.objects.get(name=ingredient)
+                            else:
+                                savedIngredient += 1
+
         print(f'Total Attempted Cocktails: {totalAttemptedItems}')
         print(f'Total Saved Drinks: {savedDrink}')
         print(f'Total Duplicate Drinks: {duplicateDrink}')
+        print(f'Total Saved Ingredients: {savedIngredient}')
+        print(f'Total Duplicate Ingredients: {duplicateIngredient}')
