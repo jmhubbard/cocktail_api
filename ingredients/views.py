a55478715d08c9685809a3029a74f770d02e7970
Jason Hubbard <jasonhubb@gmail.com>
2020-10-29 23:57:23 -0700

    create ingredientList view

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Ingredient
+from .serializers import IngredientSerializer
+
+@api_view(['GET'])
+def ingredientList(request):
+    ingredients = Ingredient.objects.all().order_by('name')
+    serializer = IngredientSerializer(ingredients, many=True)
+
+    return Response(serializer.data)
