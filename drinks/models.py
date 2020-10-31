60a5b989711372c7ca36dfc508d7bc6c0f8765b4
Jason Hubbard <jasonhubb@gmail.com>
2020-10-31 12:19:52 -0700

    add ImageField to Drink model

--- drinks/models.py
+++ drinks/models.py
@@ -6,6 +6,7 @@ class Drink(models.Model):
     name = models.CharField(max_length=200, unique=True)
     instructions = models.TextField()
     ingredients = models.ManyToManyField(Ingredient, through='recipes.Recipe')
+    image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.name
