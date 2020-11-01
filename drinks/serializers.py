04ced4d8cd85ae069807de1c9b4d6aef717dc97e
Jason Hubbard <jasonhubb@gmail.com>
2020-10-31 22:57:48 -0700

    test to add additional infor to fields

--- drinks/serializers.py
+++ drinks/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from .models import Drink
 from recipes.serializers import RecipeSerializer
+from django.urls import reverse, reverse_lazy
+from django.http import request
 
 class DrinkSerializer(serializers.ModelSerializer):
     ingredients = RecipeSerializer(source='recipe_set', many=True)
+    drinktest = serializers.SerializerMethodField('add_text_to_drink')
     class Meta:
         model = Drink
-        fields = ('id','name','ingredients','instructions',)
+        fields = ('id','name','ingredients','instructions', 'image', 'drinktest')
+
+    def add_text_to_drink(self, drink):
+        
+        drinkname = f'http://sdlsjfsdlf/{drink.image}'
+        return drinkname
+
