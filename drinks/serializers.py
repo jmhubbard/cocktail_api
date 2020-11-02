bf088155ee801e49c410b0557a1aa3796dba659d
Jason Hubbard <jasonhubb@gmail.com>
2020-11-01 20:32:54 -0800

    removed add_text_to_drink function

--- drinks/serializers.py
+++ drinks/serializers.py
@@ -6,13 +6,7 @@ from django.http import request
 
 class DrinkSerializer(serializers.ModelSerializer):
     ingredients = RecipeSerializer(source='recipe_set', many=True)
-    drinktest = serializers.SerializerMethodField('add_text_to_drink')
     class Meta:
         model = Drink
-        fields = ('id','name','ingredients','instructions', 'image', 'drinktest')
-
-    def add_text_to_drink(self, drink):
-        
-        drinkname = f'http://sdlsjfsdlf/{drink.image}'
-        return drinkname
+        fields = ('id','name','ingredients','instructions', 'image')
 
