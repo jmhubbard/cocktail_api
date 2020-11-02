8afe12a5cd196eef2d7efc88f11309b0a9c62754
Jason Hubbard <jasonhubb@gmail.com>
2020-11-01 21:54:32 -0800

    set settings_local

---
+++ project/settings_local.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
