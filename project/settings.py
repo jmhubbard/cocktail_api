d6109554b423004da9e536063d12ff48ac12936c
Jason Hubbard <jasonhubb@gmail.com>
2020-11-01 21:45:44 -0800

    installed main app

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'drinks.apps.DrinksConfig',
     'ingredients.apps.IngredientsConfig',
     'recipes.apps.RecipesConfig',
